66ae6bb9da3d7ed070a4914bc7a464c4f0656e16
lucas <63783545+ProjetoEstagioGrupo3@users.noreply.github.com>
2020-05-20 16:30:34 +0100

    layout professores

--- Reporte_avarias/resources/views/Admin/Professores/index.blade.php
+++ Reporte_avarias/resources/views/Admin/Professores/index.blade.php
@@ -1,9 +1,49 @@
 @extends('Admin.Layouts.home')
 
 @section('content')
-<!-- Main content -->
 <section class="content">
     <h1>Professores</h1>
+    <div class="container-fluid">
+      <div class="row">
+        <!-- left column -->
+        <div class="col-md-6">
+          <!-- general form elements -->
+          <div class="card card-primary">
+            <div class="card-header">
+              <h3 class="card-title">Adicionar Professores</h3>
+            </div>
+            <!-- /.card-header -->
+            <!-- form start -->
+            <form role="form">
+              <div class="card-body">
+                <div class="form-group">
+                  <label>Numero de Processo</label>
+                  <input type="text" class="form-control" placeholder="Enter ...">
+                </div>
+                <div class="form-group">
+                  <label>Nome</label>
+                  <input type="text" class="form-control" placeholder="Enter ...">
+                </div>
+                <div class="form-group">
+                  <label>Email</label>
+                  <input type="email" class="form-control" placeholder="Enter ...">
+                </div>
+                <div class="form-group">
+                  <label>Senha</label>
+                  <input type="password" class="form-control" placeholder="Enter ...">
+                </div>
+                <div class="form-group">
+                  <label>Função</label>
+                  <select class="form-control">
+                    <option>Professor</option>
+                    <option>Equipa Tic</option>
+                    <option>Administrador</option>
+                  </select>
+                </div>
+              </div>
+              <div class="card-footer">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+            </form>
   </section>
-  <!-- /.content -->
 @endsection
